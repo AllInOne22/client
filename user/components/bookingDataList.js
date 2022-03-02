5330e43ac470de61de840d0a1a11fbe9e1c6c105
Rezgui <mohamedrezguimedrezz@gmail.com>
2022-03-02 13:29:07 +0100

    clinet

---
+++ user/components/bookingDataList.js
@@ -0,0 +1,71 @@
+export const UpcomingDataList = [
+    {
+        id: '1',
+        date: '15 March 2021',
+        time: '10:00 AM',
+        person: 'Apollonia Anderson',
+        profession: 'Cleaner'
+    },
+    {
+        id: '2',
+        date: '21 March 2021',
+        time: '12:00 PM',
+        person: 'Linnea Hayden',
+        profession: 'Cleaner'
+    },
+    {
+        id: '3',
+        date: '25 March 2021',
+        time: '04:00 PM',
+        person: 'John Smith',
+        profession: 'Plumber'
+    },
+];
+
+export const PastDataList = [
+    {
+        id: '1',
+        date: '18 March 2021',
+        time: '12:00 PM',
+        person: 'Amara Smith',
+        profession: 'Cleaner'
+    },
+    {
+        id: '2',
+        date: '22 March 2021',
+        time: '08:00 AM',
+        person: 'Shira Williamson',
+        profession: 'Cleaner'
+    },
+    {
+        id: '3',
+        date: '27 March 2021',
+        time: '03:00 PM',
+        person: 'John Smith',
+        profession: 'Electrician'
+    },
+    {
+        id: '4',
+        date: '2 April 2021',
+        time: '05:00 PM',
+        person: 'Faina Maxwell',
+        profession: 'Beautician'
+    },
+];
+
+export const CancelledDataList = [
+    {
+        id: '1',
+        date: '14 March 2021',
+        time: '04:00 PM',
+        person: 'Amara Smith',
+        profession: 'Cleaner'
+    },
+    {
+        id: '2',
+        date: '17 March 2021',
+        time: '09:00 AM',
+        person: 'Ellison Perry',
+        profession: 'Cleaner'
+    },
+];
